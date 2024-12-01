473b020e5942b8da9ee114425eb69d795a3f5532
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-01 21:29:05 +0530

    message

--- lib/FRONTEND/ADMIN/home.dart
+++ lib/FRONTEND/ADMIN/home.dart
@@ -75,107 +75,110 @@ class adminHomeState extends State<adminHome> {
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(
-                  left: 16.0), // Optional padding around the heading
-              child: Text(
-                'Health and fitness gym',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(height: 10), // Adds space between heading and paragraph
-            const Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 16.0), // Optional padding around the paragraph
-              child: Text(
-                'Kottayam',
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(height: 20), // Adds space between the paragraph and row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 100,
-                  height: 600,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(
-                        16.0), // Adjust the radius as needed
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(
-                        16.0), // Apply the same radius to the image
-                    child: Image.asset(
-                        'images/logo.jpg' // Replace with your image URL
-                        ),
-                  ),
+      body: SingleChildScrollView(
+        child: Center(
+          
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                    left: 16.0), // Optional padding around the heading
+                child: Text(
+                  'Health and fitness gym',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-
-                const SizedBox(width: 20), // Adds space between the containers
-                Column(
-                  children: [
-                    const SizedBox(width: 20), // Adds space between the containers
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      child: const Center(
-                        child: Text(
-                          'Container 2',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20), // Adds space between the containers
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      child: const Center(
-                        child: Text(
-                          'Container 2',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20), // Adds space between the containers
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      child: const Center(
-                        child: Text(
-                          'Container 2',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20), // Adds space between the containers
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.blue,
-                      child: const Center(
-                        child: Text(
-                          'Container 2',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
+              ),
+              const SizedBox(height: 10), // Adds space between heading and paragraph
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 16.0), // Optional padding around the paragraph
+                child: Text(
+                  'Kottayam',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 16),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 20), // Adds space between the paragraph and row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 600,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(
+                          16.0), // Adjust the radius as needed
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                          16.0), // Apply the same radius to the image
+                      child: Image.asset(
+                          'images/logo.jpg' // Replace with your image URL
+                          ),
+                    ),
+                  ),
+        
+                  const SizedBox(width: 20), // Adds space between the containers
+                  Column(
+                    children: [
+                      const SizedBox(width: 20), // Adds space between the containers
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.blue,
+                        child: const Center(
+                          child: Text(
+                            'Container 2',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+        
+                      const SizedBox(height: 20), // Adds space between the containers
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.blue,
+                        child: const Center(
+                          child: Text(
+                            'Container 2',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20), // Adds space between the containers
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.blue,
+                        child: const Center(
+                          child: Text(
+                            'Container 2',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20), // Adds space between the containers
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.blue,
+                        child: const Center(
+                          child: Text(
+                            'Container 2',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
