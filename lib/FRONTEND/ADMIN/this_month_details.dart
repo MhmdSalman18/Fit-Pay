8f1327f383379f72c18996e221ad7d491995f260
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-03 19:53:56 +0530

    message

---
+++ lib/FRONTEND/ADMIN/this_month_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThisMonthDetails extends StatefulWidget {
+  const ThisMonthDetails({super.key});
+
+  @override
+  State<ThisMonthDetails> createState() => _ThisMonthDetailsState();
+}
+
+class _ThisMonthDetailsState extends State<ThisMonthDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
