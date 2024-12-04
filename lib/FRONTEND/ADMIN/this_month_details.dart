b09581bbc8a1a37a12d68b3fa7db4fdf92887963
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-04 19:18:40 +0530

    message

--- lib/FRONTEND/ADMIN/this_month_details.dart
+++ lib/FRONTEND/ADMIN/this_month_details.dart
@@ -11,7 +11,7 @@ class _ThisMonthDetailsState extends State<ThisMonthDetails> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      
+      appBar: AppBar(),
     );
   }
 }
